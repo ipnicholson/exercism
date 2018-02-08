bb2870b5056fa317ccda8b782250a22d3ae084a8
Ian Nicholson <ipnicholson@gmail.com>
2018-02-08 14:55:44 -0800

    green

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,10 @@
 class Gigasecond 
-  def self.from(input_time) # input_time of format Time.utc(YEAR, MM, DD, HH, mm, SS)
+  def self.from(input_time) # input_time format: Time.utc(YEAR, MM, DD, HH, mm, SS)
     Time.at(input_time.to_i + 1000000000)
   end
+end
+
+module BookKeeping
+  VERSION = 6
 end
 
