d03a9f575e4be48acb1bd4c5ade07ec5dfb656cb
Ian Nicholson <ipnicholson@gmail.com>
2018-02-09 09:16:11 -0800

    refactor

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,25 +1,17 @@
 class Complement
-  KEY = {
+  DNA_TO_RNA = {
     :G => "C",
     :C => "G",
     :T => "A",
     :A => "U"
   }
   
-  def self.of_dna(input)
-    result = ''
-    
-    input.each_char do |char|
-      translation = KEY[char.to_sym]
-      if translation 
-        result += translation
-      else
-        result = ''
-        break
-      end
-    end
-    
-    result
+  def self.of_dna(strand)
+    strand.chars.map do |letter| 
+      DNA_TO_RNA.fetch(letter.to_sym)
+    end.join
+  rescue KeyError
+    ''
   end
 end
 
