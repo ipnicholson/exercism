3dd89b6cb73e2321599d8a2fc7451b0e49a19cc3
Ian Nicholson <ipnicholson@gmail.com>
2018-02-11 22:43:46 -0800

    green

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,16 @@
-class Hamming
-  def self.compute(a, b)
-    if a.length == b.length
-      count = 0
-      a.each_char, b.each_char do |a_char, b_char|
-        if (a_char != b_char)
-          count +=1
-        end
-        count
-      end
-    else
-      puts "inputs need to be same length"
-    end
+module Hamming
+  def self.compute(str_1, str_2)
+    raise ArgumentError if (str_1.length != str_2.length)
+
+    arr_1 = str_1.split('')
+    arr_2 = str_2.split('')
+
+    comparison = arr_1.zip(arr_2).map { |x, y| x == y }
+
+    comparison.count(false)
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
